43484f99c89d9457aedf67e71369871d96501c81
Mohammad <mfarahani@ce.sharif.edu>
2022-08-27 17:20:21 +0430

    code for experiments

--- Code/Arduino/test/test.ino
+++ Code/Arduino/test/test.ino
@@ -1,14 +1,8 @@
-const unsigned int TRIG_PIN=11;
-const unsigned int ECHO_PIN=10;
+const unsigned int TRIG_PIN=13;
+const unsigned int ECHO_PIN=12;
 const unsigned int BAUD_RATE=9600;
 
-void setup() {
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
-  Serial.begin(BAUD_RATE);
-}
-
-void loop() {
+void distance_detector(unsigned int TRIG_PIN, unsigned int ECHO_PIN){
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH);
@@ -19,12 +13,26 @@ void loop() {
  const unsigned long duration= pulseIn(ECHO_PIN, HIGH);
  int distance= duration/29/2;
  if(duration==0){
-   Serial.println("Warning: no pulse from sensor");
+   Serial.print("Warning: no pulse from sensor");
    } 
   else{
-      Serial.print("distance to nearest object:");
-      Serial.println(distance);
+      Serial.print(duration);
+      Serial.print(" s-sensor distance to nearest object:");
+      Serial.print(distance);
       Serial.println(" cm");
   }
- delay(100);
+}
+
+void setup() {
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  Serial.begin(BAUD_RATE);
+}
+
+void loop() {
+  
+  distance_detector(TRIG_PIN, ECHO_PIN);
+
+  delay(200);
+  
  }
